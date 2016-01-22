d17892f5fbdc166af11d5458fb2ebf72d5cc6678
NealRame <julien@graziano.fr>
2016-01-22 02:13:23 +0100

    Adapt Gruntfile for metalsmith and sass.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,34 +1,98 @@
 /* eslint-env node*/
 /* eslint-disable strict*/
 module.exports = function(grunt) {
+
+    function is_dev() {
+        switch (process.env.NODE_ENV) {
+        case 'development':
+        case 'dev':
+            return true;
+        default:
+            return false;
+        }
+    }
+
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-
         ///////////////////////////////////////////////////////////////////////
         // Variables
         sources_dir: 'src',
+        pages_layouts_dir: '<%= sources_dir %>/layouts',
+        pages_partials_dir: '<%= pages_layouts_dir %>/partials',
+        pages_sources_dir: '<%= sources_dir %>/pages',
+        sass_sources_dir: '<%= sources_dir %>/sass',
         public_dir: 'public',
-        assets_dir: '<%= public_dir %>/assets',
-        templates_dir: '<%= sources_dir %>/templates',
-        partials_dir: '<%= templates_dir %>/partials',
-        pages_dir: '<%= public_dir %>/pages',
-
+        pages_dest_dir: '<%= public_dir %>/pages',
+        style_dest_dir: '<%= public_dir %>/css',
         ///////////////////////////////////////////////////////////////////////
         // Configure tasks
-        assemble: {
-            options: {
-                assets: '<%= assets_dir %>',
-                data: '<%= sources_dir %>/config.json',
-                layout: '<%= templates_dir %>/default.hbs',
-                partials: '<%= partials_dir>/**/*.hbs>'
+        clean: {
+            'pages': ['<%= pages_dest_dir %>'],
+            'style': ['<%= style_dest_dir %>']
+        },
+        metalsmith: {
+            pages: {
+                options: {
+                    metadata: {
+                    },
+                    plugins: [
+                        {'metalsmith-markdown': {}},
+                        {'metalsmith-layouts': {
+                            directory: '<%= pages_layouts_dir %>',
+                            engine: 'handlebars',
+                            partials: '<%= pages_partials_dir %>',
+                            default: 'cv.html'
+                        }}
+                    ]
+                },
+                src: '<%= pages_sources_dir %>',
+                dest: '<%= pages_dest_dir %>'
+            }
+        },
+        sass: {
+            compile: {
+                options: {
+                    outputStyle: is_dev() ? 'nested' : 'compressed',
+                    sourceMap: is_dev()
+                },
+                files: [{
+                    expand: true,
+                    cwd: '<%= sass_sources_dir %>',
+                    src: ['style.scss'],
+                    dest: '<%= style_dest_dir %>',
+                    ext: '.css'
+                }]
+            }
+        },
+        watch: {
+            pages: {
+                files: [
+                    '<%= pages_sources_dir %>/**/*',
+                    '<%= pages_layouts_dir %>/**/*'
+                ],
+                tasks: ['metalsmith:pages'],
+                options: {
+                    spawn: true
+                }
             },
-            cv: {
-                src: '<%= sources_dir %>/cv.hbs',
-                dest: '<%= public_dir %>'
+            style: {
+                files: [
+                    '<%= sass_sources_dir %>/**/*.scss',
+                    '<%= sass_sources_dir %>/**/*.sass'
+                ],
+                tasks: ['sass'],
+                options: {
+                    spawn: true
+                }
             }
         }
     });
-    grunt.loadNpmTasks('assemble' );
-    grunt.loadNpmTasks('grunt-newer' );
-    grunt.registerTask('default', ['newer:assemble' ]);
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-metalsmith');
+    grunt.loadNpmTasks('grunt-notify');
+    grunt.loadNpmTasks('grunt-sass');
+    ///////////////////////////////////////////////////////////////////////
+    // Register macro task(s).
+    grunt.registerTask('default', ['clean', 'metalsmith:pages', 'sass']);
 };
