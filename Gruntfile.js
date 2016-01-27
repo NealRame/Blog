41268025f07e4c1c0f99174c7028e120ad2fa31e
NealRame <julien@graziano.fr>
2016-01-27 23:03:44 +0100

    Add browserify target.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,27 @@
-/* eslint-env node*/
-/* eslint-disable strict*/
+'use strict';
+
+/* eslint-env node, es6 */
+
+const _ = require('underscore');
+const fs = require('fs');
+const path = require('path');
+
+// sources variables
+const sources_dir = 'src';
+const content_dir = path.join(sources_dir, 'content');
+const pages_dir = path.join(content_dir, 'pages');
+const layouts_dir = path.join(sources_dir, 'layouts');
+const helpers_dir = path.join(sources_dir, 'helpers');
+const partials_dir = path.join(layouts_dir, 'partials');
+const sass_sources_dir = path.join(sources_dir, 'sass');
+const scripts_sources_dir = path.join(sources_dir, 'scripts');
+
+// destinations variables
+const dest_dir = 'public';
+const assets_dest_dir = path.join(dest_dir, 'assets');
+const style_dest_dir = path.join(assets_dest_dir, 'css');
+const scripts_dest_dir = path.join(assets_dest_dir, 'scripts');
+
 module.exports = function(grunt) {
 
     function is_dev() {
@@ -12,25 +34,49 @@ module.exports = function(grunt) {
         }
     }
 
+    function is_prod() {
+        return !is_dev()
+    }
+
+    function create_target(app, apps_source_dir, apps_dest_dir) {
+        const sources_dir = path.join(apps_source_dir, app);
+        const dest_dir = path.join(apps_dest_dir, app);
+        const options = Object.assign(
+            {browserifyOptions: {debug: true}},
+            is_prod() ? {plugin: [['minifyify', {map: false, minify: true}]]} : {}
+        );
+        return _.object([[app, {
+            options,
+            src: [`${sources_dir}/main.js`],
+            dest: `${dest_dir}/app.js`
+        }]]);
+    }
+
+    function create_browserify_targets(apps_sources_dir, apps_dest_dir) {
+        return fs.readdirSync(apps_sources_dir)
+            .filter((entry) => fs.statSync(path.join(apps_sources_dir, entry)).isDirectory())
+            .reduce((targets, app) => Object.assign(
+                targets,
+                create_target(app, apps_sources_dir, apps_dest_dir)
+            ), {});
+    }
+
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         ///////////////////////////////////////////////////////////////////////
         // Variables
-        // sources variables
-        sources_dir: 'src',
-        content_dir: '<%= sources_dir %>/content',
-        pages_dir: '<%= content_dir %>/pages',
-        layouts_dir: '<%= sources_dir %>/layouts',
-        helpers_dir: '<%= sources_dir %>/helpers',
-        partials_dir: '<%= layouts_dir %>/partials',
-        sass_sources_dir: '<%= sources_dir %>/sass',
-        scripts_sources_dir: '<%= sources_dir %>/scripts',
-        // destination variables
-        public_dir: 'public',
-        dest_dir: '<%= public_dir %>',
-        assets_dest_dir: '<%= dest_dir %>/assets',
-        style_dest_dir: '<%= assets_dest_dir %>/css',
-        scripts_dest_dir: '<%= assets_dest_dir %>/scripts',
+        sources_dir,
+        content_dir,
+        pages_dir,
+        layouts_dir,
+        helpers_dir,
+        partials_dir,
+        sass_sources_dir,
+        scripts_sources_dir,
+        dest_dir,
+        assets_dest_dir,
+        style_dest_dir,
+        scripts_dest_dir,
         ///////////////////////////////////////////////////////////////////////
         // Configure tasks
         clean: {
@@ -87,6 +133,7 @@ module.exports = function(grunt) {
                 }]
             }
         },
+        browserify: create_browserify_targets(scripts_sources_dir, scripts_dest_dir),
         uglify: {
             options: {
                 sourceMap: is_dev()
