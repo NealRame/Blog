5add8abac60d4eceb8b45d7ac84860f3aa9be78f
NealRame <julien@graziano.fr>
2016-01-27 23:13:23 +0100

    Remove dependency.
    - runt-contrib-uglify: ^0.11.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -168,7 +168,6 @@ module.exports = function(grunt) {
         }
     });
     grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-metalsmith');
     grunt.loadNpmTasks('grunt-notify');
