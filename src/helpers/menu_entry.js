9e64a72d0167edee38d04679fd7c9dabe77b9a89
NealRame <julien@graziano.fr>
2016-01-27 13:28:41 +0100

    Add handlebars helpers directory.

---
+++ src/helpers/menu_entry.js
@@ -0,0 +1,18 @@
+const Handlebars = require('handlebars');
+
+const templates = {
+    active: Handlebars.compile('<li class="active">{{{content}}}</li>'),
+    unactive: Handlebars.compile('<li><a href="{{link}}">{{{content}}}</a></li>')
+};
+
+module.exports = function(entries, current_page) {
+    const items = entries.map((entry) => {
+        const context = entry.page === '/'
+            ? {content: '<i class="fa fa-home"></i>', link: '/'}
+            : {content: entry.page, link: `/pages/${entry.page}.html`};
+        return entry.page === current_page
+            ? templates.active(context)
+            : templates.unactive(context);
+    }).join('');
+    return `<ul>${items}</ul>`;
+}
