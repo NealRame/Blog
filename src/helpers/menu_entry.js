04ef7f7e2ddae2e7a5bbef4973acf881ed9b1360
NealRame <julien@graziano.fr>
2016-03-17 21:00:41 +0100

    refactor 'menu_entry' helper

--- src/helpers/menu_entry.js
+++ src/helpers/menu_entry.js
@@ -1,22 +1,28 @@
 const Handlebars = require('handlebars');
+const href = require('./href');
 
 const templates = {
     active: Handlebars.compile('<li class="active"><a>{{{content}}}</a></li>'),
-    unactive: Handlebars.compile('<li><a href="{{link}}">{{{content}}}</a></li>')
+    unactive: Handlebars.compile('<li><a {{{link}}}>{{{content}}}</a></li>')
 };
 
 module.exports = function(entries, current_page) {
-    const items = entries
+    return entries
         .filter((entry) => !!entry.menu)
         .sort((a, b) => a['menu-rank'] - b['menu-rank'])
+        .map((entry) => Object.assign(
+            {
+                page: entry.page,
+                link: href(entry)
+            },
+            entry.page === '/'
+                ? {content: '<i class="fa fa-home"></i>'}
+                : {content: entry.page}
+        ))
         .map((entry) => {
-            const context = entry.page === '/'
-                ? {content: '<i class="fa fa-home"></i>', link: '/'}
-                : {content: entry.page, link: `/pages/${entry.page}.html`};
             return entry.page === current_page
-                ? templates.active(context)
-                : templates.unactive(context);
+                ? templates.active(entry)
+                : templates.unactive(entry);
         })
         .join('');
-    return `<ul class="menu">${items}</ul>`;
 }
