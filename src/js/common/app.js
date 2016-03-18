239ceb0e6afd2e974223717d35b7c4aabf7ebb42
NealRame <julien@graziano.fr>
2016-03-18 13:19:42 +0100

    stick footer to bottom of screen

--- src/js/common/app.js
+++ src/js/common/app.js
@@ -1,9 +1,24 @@
+import {debounce} from 'underscore';
 import $ from 'jquery';
 import 'foundation';
+
+function stick_footer(footer) {
+	const rect = footer.getBoundingClientRect();
+	if (rect.bottom < window.innerHeight) {
+		footer.style.marginTop = window.innerHeight - rect.bottom + 'px';
+	}
+}
 
 export default function(run) {
 	$(window).load(() => {
 		$(document).foundation();
+		$(window)
+			.on('resize', debounce(() => {
+				const footer = $('#site-content-wrapper > footer').get(0);
+				footer.style.marginTop = 0;
+				stick_footer(footer);
+			}, 250))
+			.trigger('resize');
 		run();
 	});
 }
