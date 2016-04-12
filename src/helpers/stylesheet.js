47f4bcb47e42f238fa454751607a112b225667a4
NealRame <julien@graziano.fr>
2016-04-12 13:24:23 +0200

    handle custom page style

--- src/helpers/stylesheet.js
+++ src/helpers/stylesheet.js
@@ -7,11 +7,17 @@ const stylesheets = [
     '/assets/css/style.css'
 ];
 
-module.exports = (collection) => {
-    return ((collection || []).indexOf('posts') < 0
+function custom_page_style(page) {
+    return (page.styles || [])
+        .map((custom_style) => `/assets/css/${custom_style}/style.css`);
+}
+
+module.exports = (page) => {
+    return ((page.collection || []).indexOf('posts') < 0
         ? stylesheets
         : stylesheets.concat('/assets/css/articles/style.css')
     )
+    .concat(custom_page_style(page))
     .map((url) => `<link rel="stylesheet" href="${url}" type="text/css">`)
     .join('\n');
 };
