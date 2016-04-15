923310b3599ca73e8d8d8809e8031844e0448ef8
NealRame <contact@nealrame.com>
2016-04-15 22:46:12 +0200

    remove useless font

--- src/helpers/stylesheet.js
+++ src/helpers/stylesheet.js
@@ -1,9 +1,8 @@
 const stylesheets = [
     'https://cdn.rawgit.com/konpa/devicon/master/devicon.min.css',
     'https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css',
-    'https://fonts.googleapis.com/css?family=Oswald:700,400',
-    'https://fonts.googleapis.com/css?family=Roboto:400,300',
 	'https://fonts.googleapis.com/css?family=Roboto+Slab:400,300',
+    'https://fonts.googleapis.com/css?family=Oswald:700',
     'https://fonts.googleapis.com/css?family=Raleway:900',
     '/assets/css/style.css'
 ];
