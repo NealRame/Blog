db37f6ebb1661c3d9407972f2cfca8ef50d7d319
NealRame <contact@nealrame.com>
2016-04-15 22:45:10 +0200

    add Roboto Slab font

--- src/helpers/stylesheet.js
+++ src/helpers/stylesheet.js
@@ -3,6 +3,7 @@ const stylesheets = [
     'https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css',
     'https://fonts.googleapis.com/css?family=Oswald:700,400',
     'https://fonts.googleapis.com/css?family=Roboto:400,300',
+	'https://fonts.googleapis.com/css?family=Roboto+Slab:400,300',
     'https://fonts.googleapis.com/css?family=Raleway:900',
     '/assets/css/style.css'
 ];
