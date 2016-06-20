2cc54bf612defc6e607713fb8354786351f60943
NealRame <contact@nealrame.com>
2016-06-20 16:31:26 +0200

    try something else

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,17 +2,3 @@
 
 # exit with non-zero exit code if anything fails
 set -e
-
-echo "before npm install"
-echo "$PWD"
-echo "========================================================================"
-ls -l $PWD/node_modules
-
-npm install
-
-echo "after npm install"
-echo "$PWD"
-echo "========================================================================"
-ls -l $PWD/node_modules
-
-grunt
