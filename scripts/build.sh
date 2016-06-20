3fa1648ced4d2f2efd6ec7d487697e0e0bb83836
NealRame <contact@nealrame.com>
2016-06-20 16:48:57 +0200

    try to make travis work

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,12 +3,8 @@
 # exit with non-zero exit code if anything fails
 set -e
 
-<<<<<<< HEAD
 ls -l "$PWD"
 echo "========================================================================"
 ls -l "$PWD/node_modules"
-=======
-ls "$PWD/node_modules/.bin"
 
 $PWD/node_modules/.bin/grunt
->>>>>>> 2ee845a898a0aa91474916879c6bff4e62e63d64
